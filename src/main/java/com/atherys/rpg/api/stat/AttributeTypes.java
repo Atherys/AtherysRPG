c416324c0784453828036ece659551774ca93f8b
Liam <liamreadsspam@gmail.com>
2019-12-01 22:17:43 -0400

    Standardize name

--- src/main/java/com/atherys/rpg/api/stat/AttributeTypes.java
+++ src/main/java/com/atherys/rpg/api/stat/AttributeTypes.java
@@ -22,7 +22,7 @@ public final class AttributeTypes {
 
     public static final AttributeType CONSTITUTION = new AttributeType("constitution", CONSTITUTION_NAME, TextColors.GOLD);
 
-    public static final AttributeType DEXTERITY = new AttributeType("Dexterity", DEXTERITY_NAME, TextColors.AQUA);
+    public static final AttributeType DEXTERITY = new AttributeType("dexterity", DEXTERITY_NAME, TextColors.AQUA);
 
     public static final AttributeType INTELLIGENCE = new AttributeType("intelligence", INTELLIGENCE_NAME, TextColors.DARK_PURPLE);
 
